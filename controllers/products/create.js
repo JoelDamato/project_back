aec397818f84e4d617742e65077d83358fa31316
Jota <damatojoel25@gmail.com>
2023-11-06 11:07:55 -0300

    add query SQL

--- controllers/products/create.js
+++ controllers/products/create.js
@@ -2,42 +2,36 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 // Configuración de la conexión a la base de datos MySQL
-const connection = mysql.createConnection({
+const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-});
+};
 
 // Función para crear un producto en la base de datos
-const createProducts = (req, res) => {
+const createProduct = (req, res) => {
   const { name, description, category, image } = req.body;
 
-  // Establecer la conexión a la base de datos MySQL
-  connection.connect((err) => {
+  const connection = mysql.createConnection(dbConfig); // Crear una nueva conexión para cada solicitud
+
+  const sql = 'INSERT INTO products (name, description, category, image) VALUES (?, ?, ?, ?)';
+  const values = [name, description, category, image];
+
+  connection.query(sql, values, (err, result) => {
     if (err) {
-      console.error('Error al conectar a la base de datos:', err.message);
-      res.status(500).json({ error: 'Error al conectar a la base de datos' });
+      console.error('Error al crear el producto:', err.message);
+      res.status(500).json({ error: 'Error al crear el producto' });
     } else {
-      const sql = 'INSERT INTO products (name, description, category, image) VALUES (?, ?, ?, ?)';
-      const values = [name, description, category, image];
-
-      connection.query(sql, values, (err, result) => {
-        if (err) {
-          console.error('Error al crear el producto:', err.message);
-          res.status(500).json({ error: 'Error al crear el producto' });
-        } else {
-          const productId = result.insertId;
-          res.json({ id: productId, name, description, category, image });
-        }
-
-        // Cerrar la conexión con la base de datos MySQL
-        connection.end();
-      });
+      const productId = result.insertId;
+      res.json({ id: productId, name, description, category, image });
     }
+
+    // Cerrar la conexión con la base de datos MySQL después de realizar la consulta
+    connection.end();
   });
 };
 
 module.exports = {
-  createProducts,
+  createProduct,
 };
--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const readController = require('../controllers/products/read');
-const createController = require('../controllers/products/create');
+const createController = require('../controllers/products/create.js');
 const deleteController = require('../controllers/products/delete');
 const readIdController = require('../controllers/products/id');
 
 router.get('/', readController.getProducts);
-router.post('/new', createController.createProducts);
+router.post('/new', createController.createProduct);
 router.delete('/delete/:id', deleteController.deleteProduct);
 router.get('/:id', readIdController.getProductById);
 
